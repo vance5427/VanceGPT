f25cad3847dd5d28c96b0fc11e0291ba666ce369
Vance Wood <48934755+vance5427@users.noreply.github.com>
2023-04-28 11:01:12 -0400

    Got basic Vancebot working
    In OpenAi-quickstart-node. i made modifications to make a basic Vancebot. Next step is to upload conversational data and train model further. It currently just has raw fact data about Vance.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=1, max_tokens=7)
 
-print("Hello, world!")
+print(response.choices[0].text)
